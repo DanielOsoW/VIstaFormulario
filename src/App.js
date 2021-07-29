5b75416153ee9be02e8393370c3e3ead03447f4e
Jordan Godoy <jordan.godoy@usach.cl>
2021-07-29 19:57:16 -0400

    Se creo la pagina para el formulario y se definio la ruta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React,{Component } from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import './App.css';
+import Formulario from './pages/Formulario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Route exact path="/" render = {() => {
+          return (
+            <div>
+                <h1>Hola</h1>
+            </div>
+          )
+        }}>
+        </Route>
+        <Route path="/formulario" render = {() => {
+          return (
+            <div>
+                <Formulario></Formulario>
+            </div>
+          )
+        }}>
+        </Route>
+    </Router>    
   );
 }
 
---
+++ src/pages/Formulario.js
@@ -0,0 +1,15 @@
+import React,{Component,useState} from 'react';
+import axios from 'axios';
+import './Formulario.css';
+
+function Formulario() {
+  return (
+    <div className="A">
+      <header >
+        wena
+      </header>
+    </div>
+  );
+}
+
+export default Formulario;
